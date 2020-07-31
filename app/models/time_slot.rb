a16088e7cba9683becd682df0a41ca81718f6cd5
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-07-31 11:46:14 -0500

    validations and seeds working: todo add timeslot/tech to workorder seeds

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,4 +1,3 @@
 class TimeSlot < ApplicationRecord
   belongs_to :technician
-  belongs_to :work_order
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,21 +12,33 @@ technicians_filepath = 'technicians.csv'
 locations_filepath = 'locations.csv'
 work_orders_filepath = 'work_orders.csv'
 
-puts 'Reseting database........'
+puts 'Deleting old information from database........'
 
+TimeSlot.delete_all
 WorkOrder.delete_all
 Technician.delete_all
 Location.delete_all
 
 
-puts 'Database Reset, Seeding Technicians.....'
+puts 'Database clean, Seeding Technicians.....'
 
 CSV.foreach(technicians_filepath, csv_options) do |row|
   Technician.find_or_create_by(id: row[0], name: row['name'] )
 end
 
-puts 'Technicians added to database'
-puts 'Seeding Locations.......'
+puts 'Technicians added to database, seeding time slots........'
+
+Technician.all.each do |tech|
+  start_time = DateTime.strptime( '10/1/2019 4:00', '%m/%d/%Y %k:%M')
+  end_time = DateTime.strptime( '10/1/2019 22:00', '%m/%d/%Y %k:%M')
+
+  until start_time == end_time
+    TimeSlot.create(start_time: start_time, end_time: (start_time + 5.minutes), duration: 5, technician_id: tech.id)
+    start_time += 5.minutes
+  end
+end
+
+puts 'Time slots added to database, seeding locations.......'
 
 CSV.foreach(locations_filepath, csv_options) do |row|
   Location.find_or_create_by(
@@ -34,16 +46,14 @@ CSV.foreach(locations_filepath, csv_options) do |row|
 end
 
 puts 'Locations added to database'
-puts 'Seeding Work Orders......'
+# puts 'Seeding Work Orders......'
 
 CSV.foreach(work_orders_filepath, csv_options) do |row|
-  time = DateTime.strptime( row['time'].insert(5, '20'), '%m/%d/%Y %k:%M')
-  WorkOrder.find_or_create_by( id: row[0], technician_id: row['technician_id'],
-                              location_id: row['location_id'], time: row['time'],
-                              duration: row['duration'], price: row['price']
-                              )
+  start_time = DateTime.strptime( row['time'].insert(5, '20'), '%m/%d/%Y %k:%M')
+  WorkOrder.find_or_create_by( id: row[0], location_id: row['location_id'], start_time: start_time,
+                              duration: row['duration'], price: row['price'], end_time: start_time + 5.minutes)
 end
 
 
-puts 'Work Orders added to database'
-puts 'Database succesfully seeded'
+# puts 'Work Orders added to database'
+# puts 'Database succesfully seeded'
