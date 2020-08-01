1679c6ab4484e56e9eee2e6fd018a6f43c6122fc
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-01 14:52:26 -0500

    range method - todo - add start / end time if no booked time slot found

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -4,19 +4,17 @@ class TimeSlot < ApplicationRecord
   def range(time_slot_id)
     current_time_slot = TimeSlot.find(time_slot_id)
     technician = current_time_slot.technician
-    technician_booked_time_slots = []
-
-    technician.time_slots.each do |ts|
-      technician_booked_time_slots << ts if ts.is_booked?
+    booked_before = []
+    booked_after = []
+    technician.time_slots.each do |time_slot|
+      if time_slot.is_booked?
+        time_slot.start_time < current_time_slot.start_time ? booked_before << time_slot : booked_after << time_slot
+      end
     end
-        # find previous work order
-      #save previous work order end time
+    range_start_time = booked_before.sort_by{ |ts| ts.end_time}.first.end_time
+    range_end_time = booked_after.sort_by{ |ts| ts.start_time}.first.end_time
 
-    #find next work order
-      #find next work order start time
-
-    # subtract next start time - previous end time
-
+    range = (range_end_time - range_start_time)/1.hour
+    range
   end
-
 end
