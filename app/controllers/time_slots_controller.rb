a7849f94767f900946eb3a8c344eb0f3ae9aee56
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-07-31 20:45:55 -0500

    refactor

--- app/controllers/time_slots_controller.rb
+++
@@ -1,6 +0,0 @@
-# class TimeSlotsController < ApplicationController
-#   def index
-#     @workorders = WorkOrder.all
-#     @technicians = Technician.all
-#   end
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'work_orders#index'
-  # get 'work_orders/index'
-  # resources :time_slots, only: [ :index ] do
-    resources :work_orders, only: [ :index ]
-  # end
+  resources :work_orders, only: [ :index ]
+
 end
