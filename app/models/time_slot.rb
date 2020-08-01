6b7fa401e117615230b7f9ffb7db526c6dd9b92d
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-01 11:11:21 -0500

    row hour headers added

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,8 +2,16 @@ class TimeSlot < ApplicationRecord
   belongs_to :technician
 
   def range(time_slot_id)
-    time_slot = TimeSlot.find(time_slot_id)
-    time_slot.duration
+    current_time_slot = TimeSlot.find(time_slot_id)
+    technician = current_time_slot.technician_id
+    technician
+    # find previous work order
+      #save previous work order end time
+
+    #find next work order
+      #find next work order start time
+
+    # subtract next start time - previous end time
 
   end
 
