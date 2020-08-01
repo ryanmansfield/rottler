7f0c8f2f3c13cbed9463be1ee3cc62a913ec3d9f
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-01 13:33:36 -0500

    range method in progress

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -3,9 +3,13 @@ class TimeSlot < ApplicationRecord
 
   def range(time_slot_id)
     current_time_slot = TimeSlot.find(time_slot_id)
-    technician = current_time_slot.technician_id
-    technician
-    # find previous work order
+    technician = current_time_slot.technician
+    technician_booked_time_slots = []
+
+    technician.time_slots.each do |ts|
+      technician_booked_time_slots << ts if ts.is_booked?
+    end
+        # find previous work order
       #save previous work order end time
 
     #find next work order
