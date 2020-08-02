0097843167b61695f31c94f657b0b870fd21b54b
Ryan Mansfield <ryanmansfield86@gmail.com>
2020-08-02 15:21:45 -0500

    formated modal content

--- app/javascript/components/time_slot_modal.js
+++ app/javascript/components/time_slot_modal.js
@@ -6,13 +6,17 @@ const timeSlotModal = () => {
       // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
       // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
       var modal = $(this)
-      // modal.find('.modal-body').text('New message to ')
-      console.log(timeSlot.id)
-
-      modal.find('.modal-title').text('There is ' + timeSlot + ' in this time slot.' )
-      // modal.find('.modal-body input').val(recipient)
+      const time = timeSlot.substr(3, 7)
+      const hours = timeSlot.substr(3)[1]
+      const minutes = timeSlot.substr(6,7)
+      if (hours === '0') {
+        modal.find('.modal-title').text('There is ' + minutes + ' minutes in this time slot.' )
+      } else if (hours === '1') {
+        modal.find('.modal-title').text('There is ' + hours + ' hour and ' + minutes + ' minutes in this time slot.' )
+      } else {
+        modal.find('.modal-title').text('There is ' + hours + ' hours and ' + minutes + ' minutes in this time slot.' )
+      }
     });
-
   });
 }
 
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -1,34 +1,34 @@
 class TimeSlot < ApplicationRecord
   belongs_to :technician
 
-  def range_duration(time_slot_id)
-    current_time_slot = TimeSlot.find(time_slot_id)
-    technician = current_time_slot.technician
-    booked_before = []
-    booked_after = []
-    technician.time_slots.each do |time_slot|
-      if time_slot.is_booked?
-        time_slot.start_time < current_time_slot.start_time ? booked_before << time_slot : booked_after << time_slot
-      end
-    end
-    find_range(booked_before, booked_after)
-  end
+  # def range_duration(time_slot_id)
+  #   current_time_slot = TimeSlot.find(time_slot_id)
+  #   technician = current_time_slot.technician
+  #   booked_before = []
+  #   booked_after = []
+  #   technician.time_slots.each do |time_slot|
+  #     if time_slot.is_booked?
+  #       time_slot.start_time < current_time_slot.start_time ? booked_before << time_slot : booked_after << time_slot
+  #     end
+  #   end
+  #   find_range(booked_before, booked_after)
+  # end
 
-  private
+  # private
 
-  def find_range(booked_before, booked_after)
-    range_start_time = find_start_time(booked_before)
-    range_end_time = find_end_time(booked_after)
+  # def find_range(booked_before, booked_after)
+  #   range_start_time = find_start_time(booked_before)
+  #   range_end_time = find_end_time(booked_after)
 
-    range = range_end_time.to_i - range_start_time.to_i
-    Time.at(range).utc.strftime('%H:%M')
-  end
+  #   range = range_end_time.to_i - range_start_time.to_i
+  #   Time.at(range).utc.strftime('%H:%M')
+  # end
 
-  def find_start_time(booked_before)
-    booked_before.count.positive? ? booked_before.max_by(&:end_time).end_time : DateTime.parse('10/1/2019 6:00')
-  end
+  # def find_start_time(booked_before)
+  #   booked_before.count.positive? ? booked_before.max_by(&:end_time).end_time : DateTime.parse('10/1/2019 6:00')
+  # end
 
-  def find_end_time(booked_after)
-    booked_after.count.positive? ? booked_after.min_by(&:start_time).start_time : DateTime.parse('10/1/2019 17:00')
-  end
+  # def find_end_time(booked_after)
+  #   booked_after.count.positive? ? booked_after.min_by(&:start_time).start_time : DateTime.parse('10/1/2019 17:00')
+  # end
 end
